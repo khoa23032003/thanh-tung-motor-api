336cb42ef5baaa6bdb4321812621521672e51432
HTuan <dangvohoangtuan0404@gmail.com>
2026-03-11 21:32:21 +0700

    feat(swagger-api): setup swagger documentation for API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\BrandsController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 //route thêm
 Route::apiResource('brands', BrandsController::class);
+
+Route::get('/test', [TestController::class, 'index']);
